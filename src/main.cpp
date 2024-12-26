cbb0be0b8cb583acafc207a791790ce4da319cac
Barwe, Leon <lbarwe.business@gmail.com>
2024-12-26 04:59:40 +0100

    NEW: add gitignore and set up cmake and the sdl2 library

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/**
+ * ###############################################################################
+ *  File: main.cpp
+ *  Project: 2D-RPG-Game
+ *  Created Date: Th 26.December 2024, 3:25:20 am
+ *  Author: lbarwe
+ *  -----
+ *  Last Modified: Th 26.December 2024, 4:42:00 am
+ *  Modified By: lbarwe
+ *  -----
+ *  Copyright (c) 2024 Leon Barwe - lbarwe.business@gmail.com
+ * ###############################################################################
+ */
+
+#define SDL_MAIN_HANDLED
+#include <iostream>
+#include "../libs/SDL2/include/SDL.h"
+
+int main(int argc, char* argv[]) 
+{
+    std::cout << "Hello World!" << std::endl;
+}
