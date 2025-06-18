da8393324dc404dea0bca8f16daebf06f50fad9f
Barwe, Leon <lbarwe.business@gmail.com>
2025-06-18 14:32:31 +0200

    FIX: refactor entities library bei adjusting comments, variable names, etc.

--- sources/entities/characters/include/character.h
+++ sources/entities/characters/include/character.h
@@ -5,7 +5,7 @@
  *  Created Date: Sa 01.March 2025, 2:52:12 pm
  *  Author: lbarwe
  *  -----
- *  Last Modified: Mo 10.March 2025, 1:50:26 pm
+ *  Last Modified: We 18.June 2025, 10:42:14 am
  *  Modified By: lbarwe
  *  -----
  *  Copyright (c) 2025 Leon Barwe - lbarwe.business@gmail.com
@@ -23,7 +23,7 @@ namespace gfx
 {
   namespace render
   {
-     class RendererManager; // forward declaration
+     class RendererManager; // TODO: Muss diese sein? forward declaration
   }
 }
 
@@ -33,27 +33,28 @@ namespace entities
     {
         /**
          * @brief Character base class for creating characters.
+         * Inherits from class Renderable.
          */
         class Character : public gfx::render::Renderable
         {
             public:
                 /**
-                 * Construct new character object.
+                 * @brief Construct new character object.
                  */
                 Character();
                 
                 /**
                  * @brief Construct new character object.
                  * Queries the texture for its width and height.
-                 * @param aStartX x-position
-                 * @param aStartY y-position
-                 * @param aTexture texture
+                 * @param aStartX start x-position
+                 * @param aStartY start y-position
+                 * @param aTexture character texture
                  */
                 Character(int aStartX, int aStartY, SDL_Texture* aTexture);
 
                 /**
                  * @brief Destroy Character object.
-                 * Destroys the texture.
+                 * Destroys the character texture.
                  */
                 ~Character();
         };
